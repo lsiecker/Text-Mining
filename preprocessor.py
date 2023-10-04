7be92332e90a6aec8c2ddb9e6b5981beeb04d743
Niels <n.p.g.t.v.beuningen@student.tue.nl>
2023-10-04 10:44:39 +0200

    small update preprocessor

--- preprocessor.py
+++ preprocessor.py
@@ -242,7 +242,11 @@ class Preprocessor:
             ents = []
             for start, end, label in annotations:
                 span = doc.char_span(start, end, label=label)
-                if span is not None:
+                if span is None:
+                    msg = f"Skipping entity [{start}, {end}, {label}] in the following text because the character span '{doc.text[start:end]}' does not align with token boundaries:\n\n{repr(text)}\n"
+                    if warn == True:
+                        warnings.warn(msg)
+                else:
                     ents.append(span)
             doc.ents = ents
             dev_db.add(doc)
